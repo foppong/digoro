b6c418451c86e90221ff293ee3ea6b2cb9810515
Frank Oppong <foppong@gmail.com>
2012-06-30 01:43:33 -0700

    fbook

--- player/player_home.php
+++ player/player_home.php
@@ -14,38 +14,14 @@
   <h3>Stuffed Cookies</h3>
   <p>
     <img title="Stuffed Cookies" 
-         src="http://fbwerks.com:8000/zhen/cookie.jpg" 
+         src="../css/imgs/digoro_splash 800x600.jpg" 
          width="550"/>
   </p>
 
-<div id="player-menu">
-	<ul id="info-nav">
-		<li><a href="player_home.php">Roster</a></li>
-	    <li><a href="player_home.php">Schedule</a></li>
-	    <li><a href="../core/account.php">SquadFill</a></li>
-	</ul>
-</div><br />
+  <br>
+  <form>
+    <input type="button" value="Cook" onclick="postCook()" />
+  </form>
 
-<p style="font-family:times;color:green;">
-	More updates to come
-</p>
-
-<form action="roster.php" method="post" id="ViewRosterFormPlayer">	
-	<p id="teamP"><b>Select Team:</b>
-	<select name="y" id="y">
-		<option value=""> - Select Team - </option>
-		<option value="1">Makan Bwya</option>
-		<option value="6">Inter Alameda</option>
-		<option value="7">Soccer Junkies</option>
-		<option value="9">Alianza</option>
-		<option value="13">Energy Strikers</option>
-		<option value="14">Trial Team</option>
-		<option value="15">Energy!!!!!</option>
-		<option value="16">Juarez</option>
-	</select>
-	<span class="errorMessage" id="teamPError">You must select your team.</span></p>		
-	
-	<div align="center"><input type="submit" name="submit" value="View Roster" />
-</form><br /><br />
 
 <?php include '../includes/footer.html'; ?>
