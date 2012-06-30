e839e27b44d79dbc30b223bef893280333a4a6fb
Frank Oppong <foppong@gmail.com>
2012-06-29 23:52:55 -0700

    facebook integration

--- player/player_home.php
+++ player/player_home.php
@@ -3,45 +3,20 @@
 	// This is the Manager Homepage
 	require '../includes/config.php';
 	$page_title = 'Welcome to digoro!';
-	include '../includes/header.html';
+	include '../includes/f_header.html';
 
 	// autoloading of classes
 	function __autoload($class) {
 		require_once('../classes/' . $class . '.php');
 	}
 
-	// Assign user object from session variable
-	if (isset($_SESSION['userObj']))
-	{
-		$user = $_SESSION['userObj'];
-	}
-	else 
-	{
-		session_unset();
-		session_destroy();
-		$url = BASE_URL . 'index.php';
-		ob_end_clean();
-		header("Location: $url");
-		exit();	
-	}
-
-	// Site access level -> Player
-	$lvl = 'P'; 
-
-	// Authorized Login Check
-	if (!$user->valid($lvl))
-	{
-		session_unset();
-		session_destroy();
-		$url = BASE_URL . 'index.php';
-		ob_end_clean();
-		header("Location: $url");
-		exit();	
-	}
-
-	echo '<h2> Hello! This is your player homepage.</h2>'; 
-
 ?>
+  <h3>Stuffed Cookies</h3>
+  <p>
+    <img title="Stuffed Cookies" 
+         src="http://fbwerks.com:8000/zhen/cookie.jpg" 
+         width="550"/>
+  </p>
 
 <div id="player-menu">
 	<ul id="info-nav">
